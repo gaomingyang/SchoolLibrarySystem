180ca2d45a60cad6c587401a90c886baf787726f
Mingyang Gao <gaomingyangcn@gmail.com>
2016-07-03 10:17:49 +0800

    fixbug about db:seed

--- database/seeds/GradeSeeder.php
+++ database/seeds/GradeSeeder.php
@@ -19,7 +19,7 @@ class GradeSeeder extends Seeder
         ['id'=>2,'name'=>'二年级','order'=>50,'deleted_at'=>null],
         ['id'=>3,'name'=>'三年级','order'=>50,'deleted_at'=>null],
         ['id'=>4,'name'=>'四年级','order'=>50,'deleted_at'=>null],
-        ['id'=>5,'name'=>'五年级','order'=>50,'deleted_at'=>null],
+        ['id'=>5,'name'=>'五年级','order'=>50,'deleted_at'=>null]
         );
     }
 }
