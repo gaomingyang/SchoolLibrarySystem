c8df79cb614504614c73b0502c143201e97d8882
Mingyang Gao <gaomingyangcn@gmail.com>
2016-07-15 23:39:43 +0800

    update

--- resources/views/admin/seat/create.blade.php
+++ resources/views/admin/seat/create.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.admin')
 
+@section('css')
+<style typte="text/css">
+.btn{
+    min-width: 68px;
+}
+#seat{
+    border:3px dashed #ccc;
+    min-height: 400px;
+}
+.students{
+    margin-bottom: 80px;
+}
+</style>
+@endsection
 
 @section('content')
 <div class="page-header">
     <h3>新建座位表</h3>
 </div>
-<h4>学生{{$squad->students->count()}}人</h4>
 
-@foreach($squad->students as $student)
-    <a href="/admin/student/{{$student->id}}" class="btn
-        @if($student->gender == 1)
-            btn-info
-        @elseif($student->gender == 2)
-            btn-warning
-        @else
-            btn-default
-        @endif
-        ">{{$student->name}}</a>
-@endforeach
 
 <h4>座位表布局设置</h4>
 <form class="form-horizontal" action="" method="post">
@@ -31,17 +33,16 @@
     </div>
     <div class="form-group">
     	<label for="mark" class="control-label col-xs-12 col-sm-2 col-md-2">行列分布</label>
-    	<span class="col-xs-5 col-sm-2 col-md-2">
+    	<span class="col-xs-1 col-sm-1 col-md-1">
     		<input type="text" class="form-control" name="row" placeholder="行" id="rowNum" value="8">
     	</span>
-        <div class="col-xs-1 col-sm-1 col-md-1">
-            x
-        </div>
-        <span class="col-xs-5 col-sm-2 col-md-2">
+        <span class="col-xs-1 col-sm-1 col-md-1">
     		<input type="text" class="form-control col" name="column" placeholder="列" id="colNum" value="4">
     	</span>
-        <span class="col-xs-1 col-sm-1 col-md-1">
+        <span class="col-xs-3 col-sm-3 col-md-3">
             <button type="button" name="button" class="btn btn-default" id="makeSeatBtn">生成</button>
+            <button type="button" name="button" class="btn btn-default" >预览</button>
+            <button type="button" name="button" class="btn btn-default" onclick="print();">打印</button>
         </span>
     </div>
     <div class="form-group">
@@ -56,6 +57,23 @@
 <div id="seat">
 
 </div>
+<h4>学生{{$squad->students->count()}}人</h4>
+<div class="students">
+    @foreach($squad->students as $key => $student)
+        <a href="/admin/student/{{$student->id}}" class="btn
+            @if($student->gender == 1)
+                btn-info
+            @elseif($student->gender == 2)
+                btn-warning
+            @else
+                btn-default
+            @endif
+            ">{{$student->name}}</a>
+            @if(($key+1)%10 == 0)
+            <br>
+            @endif
+    @endforeach
+</div>
 
 
 @endsection
@@ -64,20 +82,24 @@
 @section('js')
 <script type="text/javascript">
 $(function(){
-    var stuNum = $("#stuNum").val();
+    var tabletype=$("input[name='tabletype']:selected").val();
+    var stunum = $("#stuNum").val();
     var row = $("#rowNum").val();
     var col = $("#colNum").val();
 
     $("#makeSeatBtn").click(function(){
-        // alert(stuNum);
-        // alert(row);
-        // alert(col);
+        alert(tabletype);
 
     });
 
 
 });
 
+
+
+function print(){
+
+}
 
 </script>
 
